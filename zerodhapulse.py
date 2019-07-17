b1851f8ec490f3803064cbdb6a35493ba4cb0dbd
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-07-17 10:58:50 +0530

    Update zerodhapulse.py

--- zerodhapulse.py
+++ zerodhapulse.py
@@ -6,21 +6,28 @@ user_agent ='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2
 urls=["https://pulse.zerodha.com/"]
 headers={'User-Agent':user_agent,}
 
-data=[]
+heading=[]
+news=[]
 request=urllib.request.Request(url,None,headers)
 response=urllib.request.urlopen(request)
-datum=response.read()
+data=response.read()
+soup=BeautifulSoup(data,'html.parser')
 
-soup=BeautifulSoup(datum,'html.parser')
-#head_box=soup.find('h2',attrs={'class':'title'})
 head_box=soup.select('.title')
 for i in head_box:
   head=i.text.strip()
-  data.append(head)
-for news in data:
-  print(news)
+  heading.append(head)
+#for news in data:
+ # print(news)
+soup.select('.desc')
+for j in description:
+  des=j.text.strip()
+  news.append(des)
+for descript in news:
+  print(descript)
 #soup.select('.title')
 #soup1=BeautifulSoup('<h2 class="title"</h2>')
 #print(soup1.prettify())
 #tag=soup1.h2
 #tag.contents
+
