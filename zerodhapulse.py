e87cd6b9b4d1990975fea894e82ceb18399a7a54
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-07-17 12:49:08 +0530

    Update zerodhapulse.py

--- zerodhapulse.py
+++ zerodhapulse.py
@@ -1,12 +1,11 @@
 import urllib.request
-import urllib.parse
+from urllib.parse import unquote
 import requests
 from bs4 import BeautifulSoup
 
 user_agent ='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 
-url=["https://pulse.zerodha.com/"]
-urllib.parse.quote(':')
+url=unquote(["http://pulse.zerodha.com/"])
 headers={'User-Agent':user_agent,}
 
 request=urllib.request.Request(url,None,headers)
@@ -43,6 +42,5 @@ for h in soup.find_all('h2'):
   
 for k in links:
   print(k)
-
 
 
