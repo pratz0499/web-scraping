158ee04574122fa314ff537f6f62cc3f07eec681
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-07-17 12:13:14 +0530

    Update zerodhapulse.py

--- zerodhapulse.py
+++ zerodhapulse.py
@@ -1,33 +1,48 @@
 import urllib.request
+import urllib.parse
 import requests
 from bs4 import BeautifulSoup
+
 user_agent ='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 
-urls=["https://pulse.zerodha.com/"]
+url=["https://pulse.zerodha.com/"]
+urllib.parse.quote(':')
 headers={'User-Agent':user_agent,}
 
-heading=[]
-news=[]
 request=urllib.request.Request(url,None,headers)
 response=urllib.request.urlopen(request)
 data=response.read()
 soup=BeautifulSoup(data,'html.parser')
 
+
+heading=[]
+news=[]
+links=[]
+
+
 head_box=soup.select('.title')
 for i in head_box:
   head=i.text.strip()
   heading.append(head)
-#for news in data:
- # print(news)
+  
+for j in heading:
+  print(j)
+  
+
+
 description=soup.select('.desc')
-for j in description:
-  des=j.text.strip()
+for k in description:
+  des=k.text.strip()
   news.append(des)
 for descript in news:
   print(descript)
-#soup.select('.title')
-#soup1=BeautifulSoup('<h2 class="title"</h2>')
-#print(soup1.prettify())
-#tag=soup1.h2
-#tag.contents
+
+for h in soup.find_all('h2'):
+  a=h.find('a')
+  links.append(a.attrs['href'])
+  
+for k in links:
+  print(k)
+
+
 
