f3a4672b06135ead38e31a30672767a9a98d7755
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-07-17 16:38:05 +0530

    final updates done

--- zerodhapulse.py
+++ zerodhapulse.py
@@ -1,11 +1,13 @@
 import urllib.request
+import csv
+from datetime import datetime
 from urllib.parse import unquote
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
 user_agent ='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 
-url=["http://pulse.zerodha.com/"]
+url="http://pulse.zerodha.com/"
 headers={'User-Agent':user_agent,}
 
 request=urllib.request.Request(url,None,headers)
@@ -24,8 +26,8 @@ for i in head_box:
   head=i.text.strip()
   heading.append(head)
   
-for j in heading:
-  print(j)
+#for j in heading:
+  #print(j)
   
 
 
@@ -33,15 +35,24 @@ description=soup.select('.desc')
 for k in description:
   des=k.text.strip()
   news.append(des)
-for descript in news:
-  print(descript)
+#for descript in news:
+ # print(descript)
 
 for h in soup.find_all('h2'):
   a=h.find('a')
   links.append(a.attrs['href'])
   
-for k in links:
-  print(k)
+#for k in links:
+  #print(k)
+data = pd.DataFrame(columns=["headings","news","links"])
+data["headings"]=heading
+data["news"]=news
+data["links"]=links
+data.to_csv("news.csv",index=True)
+  
+  
+
+
 
 
 
